0a8838c8770c1c9f42b288d82bcf2a011030028b
Juanita Munar <juanita.munar0.0@gmail.com>
2024-04-15 15:05:19 -0500

    ndarray de 2 dimensiones y ecuaciones numpy

--- programa.py
+++ programa.py
@@ -1,1 +1,19 @@
-#Taller Numpy 
+#Numpy: Biblioteca para realizar calculos numericos en arreglos n-dimesnioanles
+
+import numpy as np
+
+#crear ndarray 2 dimensiones a partir de una lista
+
+A = np.array([[1,5], [7,9]])
+B = np.array([[2,0], [1,3]])
+
+#Producto punto entre ndarrays
+
+C= np.dot(A,B)
+print(C)
+
+#Solucion de un sistema de ecuaciones con Numpy
+m_solucion =np.array([5,17])
+
+m=np.linalg.solve(A,m_solucion)
+print(m)
